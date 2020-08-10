5cadff44776a2a436400d40cca332126ae0fc2eb
Rodrigo Tamura <rodrigo.tamura@catho.om>
2020-08-10 12:06:39 -0300

    test CI

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -1,16 +1,4 @@
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    '<rootDir>/src/log/*.js',
-    '<rootDir>/src/controllers/*/index.js',
-    '<rootDir>/src/services/*.js',
-  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -19,7 +7,7 @@ module.exports = {
   coverageReporters: ['text-summary', 'lcov'],
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['dotenv/config'],
+  // setupFiles: ['dotenv/config'],
 
   // The test environment that will be used for testing
   testEnvironment: 'node',
